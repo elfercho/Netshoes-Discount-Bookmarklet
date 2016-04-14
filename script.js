ef2489aba3fad0d9b8e75a6416ff8d61f1239065
Fernando <feer.rt@hotmail.com>
2016-04-14 11:42:20 -0300

    Update script.js
    descuento

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ javascript:(function(){
 	for(var i=0; i<precio.length; i++){
 		var price = precio[i].innerHTML.slice(1);
 		var newPrice = price.replace('.','').replace(',','.');
-		newPrice = (newPrice*0.68).toFixed(2);
+		newPrice = (newPrice*0.8).toFixed(2);
 		var total = "$"+newPrice;
 		var cuotasFinal = (newPrice/3).toFixed(2);
 		precio[i].innerHTML = total;
